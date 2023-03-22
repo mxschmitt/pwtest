1220588c25876868ff0ac6ed8ef8dacced422797
op-13 <128555701+op-13@users.noreply.github.com>
2023-03-22 08:46:08 +0100

    .

---
+++ SimpleTest/SimpleTest/SimpleTestWithPlaywright.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTest
+{
+    [TestClass]
+    public class SimpleTestWithPlaywright
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            using (var instance = await Playwright.CreateAsync())
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+    }
+}
